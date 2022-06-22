b2730b8338f40f8bede04aa909eb313ffb46f602
Paul-Nicolas Madelaine <pnm@pnm.tf>
2022-06-22 08:00:00 +0200

    TO REVERT: don't overwrite everest-nix inputs

--- flake.nix
+++ flake.nix
@@ -1,14 +1,7 @@
 {
   inputs = {
-    fstar-src.url     = github:fstarlang/fstar/master
-    fstar-src.flake   = false;
-    karamel-src.url   = github:fstarlang/karamel/master;
-    karamel-src.flake = false;
-
     everest = {
       url = github:project-everest/everest-nix?dir=projects;
-      inputs.fstar-src.follows    = "fstar-src";
-      inputs.karamel-src.follows  = "karamel-src";
       # `/` means `self` (see NixOS/nix#4931)
       inputs.hacl-src.follows     = "/";
     };
